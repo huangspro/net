e45d539c094b1e66d774f2532186d3fa440e551a
hhy <13612276628@163.com>
2025-12-26 19:40:01 +0800

    1.0

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "begin"
+echo "add========================================"
+git add .
+echo "commit========================================="
+git commit -m "1.0"
+echo "push============================================"
+git push
+
+clear
+
+echo "finished"
