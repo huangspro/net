9732931d91807da6435265623b168d3ed82356c8
hhy <13612276628@163.com>
2025-12-27 16:50:04 +0800

    1.0

--- main.C
+++ main.C
@@ -1,46 +1,10 @@
 #include "Node.h"
+#include "net.h"
 #include<iostream>
 
 using namespace std;
 
 int main(){
-  vector<Ope*> o;
-  vector<Var*> v;
-  for(int i=0;i<9;i++){v.push_back(new Var(0,0));}
-  o.push_back(new Dev());
-  o.push_back(new Minus());
-  o.push_back(new Dev());
-  o.push_back(new Add());
-  o.push_back(new Add());
-  o.push_back(new Ln());
-  
-  v[0]->load(o[0]);
-  v[1]->load(o[1]);
-  v[2]->load(o[2]);
-  v[4-1]->load(o[3]);
-  v[5-1]->load(o[3]);
-  v[7-1]->load(o[4]);
-  v[5]->load(o[4]);
-  v[7]->load(o[5]);
-  v[8]->load(nullptr);
-  
-  o[0]->load(v[0],v[3]);
-  o[1]->load(v[1],v[4]);
-  o[2]->load(v[2],v[6]);
-  o[3]->load(v[3],v[4],v[5]);
-  o[4]->load(v[5],v[6],v[7]);
-  o[5]->load(v[7],v[8]);
-  
-  v[0]->data=1.9;
-  v[1]->data=-1.6;
-  v[2]->data=0.3;
-  v[8]->gradient=0.36;
-  for(auto i=o.begin();i!=o.end();i++){
-      (*i)->forward();
-  }
-  cout<<v[8]->data<<endl;
-  for(auto i=o.rbegin();i!=o.rend();i++){
-      (*i)->backward();
-  }
-  cout<<v[0]->gradient<<"  "<<v[1]->gradient<<"  "<<v[2]->gradient<<endl;
+  InputLayer* I = new InputLayer(10);
+  I->connect_to_nonlinear_layer(new NonlinearLayer(10,NonlinearLayer::RELU));
 }
--- net.h
+++ net.h
@@ -1,13 +1,64 @@
+#ifndef _NET_H_
+#define _NET_H_
+
 #include "Node.h"
 #include<vector>
+#include<iostream>
+
+class NonlinearLayer{
+public:
+  static const int RELU=1;
+  static const int TANH=2;
+  static const int SIGMOID=3;
+  int neuron;
+  int function_type;
+  std::vector<Var*> input, layer_output;
+  std::vector<Ope*> opes;
+  
+  NonlinearLayer(int n, int f):neuron(n),function_type(f){
+    for(int i=0;i<n;i++){
+     //create all nodes
+      input.push_back(new Var(0,0));
+      layer_output.push_back(new Var(0,0));
+      switch(f){//judge nonlinear function type
+        case RELU:
+          opes.push_back(new Relu());break;
+        case TANH:
+          opes.push_back(new Tanh());break;
+        case SIGMOID:
+          opes.push_back(new Sigmoid());break;
+      }
+      //load all operation nodes
+      opes[i]->load(input[i],layer_output[i]);
+      //load all variable nodes
+      input[i]->load(opes[i]);
+      layer_output[i]->load(opes[i]);
+    }
+  }
+  void connect_to_next_layer(){
+  
+  }
+  
+  ~NonlinearLayer(){
+    for(int i=0;i<neuron;i++){
+     delete input[i];
+     delete layer_output[i];
+     delete opes[i];
+    }
+  }
+};
+
 
 class InputLayer{
 public:
-  int neuron; //number of neuron in this layer
+  int neuron; 
   std::vector<Var*> input, weight, bias, mul_output, add_output, layer_output;
   std::vector<Ope*> mul, add;
+  
   InputLayer(int n):neuron(n){
+  
     for(int i=0;i<neuron;i++){
+      std::cout<<i<<std::endl;
       //create the Nodes
       input.push_back(new Var(0,0));
       weight.push_back(new Var(0,0));
@@ -16,7 +67,7 @@ public:
       mul_output.push_back(new Var(0,0));
       add_output.push_back(new Var(0,0));
       
-      mul.push_back(new Mull());
+      mul.push_back(new Mul());
       add.push_back(new Add());
       //load the operation nodes
       mul[i]->load(input[i],weight[i],mul_output[i]);
@@ -27,12 +78,17 @@ public:
       mul_output[i]->load(add[i]);
       bias[i]->load(add[i]);
       //load the output layer
-      layer_output(nullptr);
+      layer_output[i]->load(nullptr);
     }
   }
   
-  void connect_to_next_layer(){
-    
+  void connect_to_nonlinear_layer(NonlinearLayer* next){
+    for(int i=0;i<neuron;i++){
+      delete next->input[i];
+      next->input[i]=layer_output[i];
+      next->input[i]->load(next->opes[i]);
+      next->opes[i]->load(next->input[i],next->layer_output[i]);
+    }
   }
   
   void forward(){
@@ -43,4 +99,19 @@ public:
       (*i)->forward();
     }
   }
+  
+  ~InputLayer(){
+    for(int i=0;i<neuron;i++){
+     delete input[i];
+     delete weight[i];
+     delete bias[i];
+     delete mul_output[i];
+     delete add_output[i];
+     delete layer_output[i];
+     delete mul[i];
+     delete add[i];
+    }
+  }
 };
+
+#endif
