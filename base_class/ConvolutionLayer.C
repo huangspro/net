f923b36631e28fe3f1adc0c6aef6d74ab90fe96c
hhy <13612276628@163.com>
2026-01-07 16:51:12 +0800

    1.0

--- base_class/ConvolutionLayer.C
+++ base_class/ConvolutionLayer.C
@@ -1,16 +1,23 @@
 #include "ConvolutionLayer.h"
 
 ConvolutionLayer::ConvolutionLayer(int r, int c, int s):conkernel_row(r), conkernel_col(c), input_row(0), input_col(0), step(s){
-  if((input_row+2*-conkernel_row)%step!=0 || (input_col+2*-conkernel_col)%step!=0){
+  if((input_row-conkernel_row)%step!=0 || (input_col-conkernel_col)%step!=0){
     std::cout<< "Please resize the convolution kernel or step" << std::endl;
     exit(1);
   }else{
-    output = new std::vector<std::vector<double>>((input_row+2*-conkernel_row)/step+1, std::vector<double>((input_col+2*-conkernel_col)/step+1));
+    output = new std::vector<std::vector<double>>((input_row-conkernel_row)/step+1, std::vector<double>((input_col-conkernel_col)/step+1));
+    conkernel = new new std::vector<std::vector<double>>(r, std::vector<double>(c));
   }
 }
 
 void ConvolutionLayer::forward(){
-  
+  double tem = 0;
+  for(int i=0;i<output->size();i++){
+    for(int ii=0;ii<(*output)[0].size();ii++){
+      
+      w(i, ii, );
+    }
+  }
 }
 
 void ConvolutionLayer::backward(){
@@ -23,4 +30,8 @@ double ConvolutionLayer::g(int row, int col){
 
 void ConvolutionLayer::w(int row, int col, double data){
   (*output)[row][col] = data;
+}
+
+void ConvolutionLayer::g2(){
+  
 }
--- base_class/ConvolutionLayer.h
+++ base_class/ConvolutionLayer.h
@@ -9,11 +9,13 @@ public:
   int conkernel_row, conkernel_col, step, input_row, input_col;
   std::vector<std::vector<double>>* input;
   std::vector<std::vector<double>>* output;
+  std::vector<std::vector<double>>* conkernel;
   ConvolutionLayer(int r, int c, int s);
   void forward();
   void backward();
   void load(std::vector<std::vector<double>>* i){input = i; input_row = i->size(); input_col = (*i)[0].size();} //load data from outside
   double g(int row, int col); //get data from input
+  double g2(int row, int col); //get data from kernel
   void w(int row, int col, double data); //write data to output
 };
 
